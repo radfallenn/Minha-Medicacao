8acc273a90aa1ec6d556b1dec7d7483b5e6b5df2
radfallenn <113562142+radfallenn@users.noreply.github.com>
2026-08-15 17:35:47 -0300

    Ativa modo tela cheia imersiva

--- app/src/main/java/com/radfallenn/minhamedicacao/MainActivity.java
+++ app/src/main/java/com/radfallenn/minhamedicacao/MainActivity.java
@@ -10,6 +10,9 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -22,6 +25,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        enableFullscreen();
 
         webView = new WebView(this);
         setContentView(webView);
@@ -40,6 +44,38 @@ public class MainActivity extends Activity {
 
         requestNotificationPermissionIfNeeded();
         requestExactAlarmPermissionIfNeeded();
+    }
+
+    private void enableFullscreen() {
+        if (Build.VERSION.SDK_INT >= 30) {
+            getWindow().setDecorFitsSystemWindows(false);
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+                controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            }
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+        }
+    }
+
+    @Override
+    protected void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) enableFullscreen();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        enableFullscreen();
     }
 
     private void requestNotificationPermissionIfNeeded() {
