d43919db5c26ca9020299994e87c57b929194553
radfallenn <113562142+radfallenn@users.noreply.github.com>
2026-08-15 17:39:35 -0300

    Corrige compilacao da tela cheia

--- app/src/main/java/com/radfallenn/minhamedicacao/MainActivity.java
+++ app/src/main/java/com/radfallenn/minhamedicacao/MainActivity.java
@@ -67,7 +67,7 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void onWindowFocusChanged(boolean hasFocus) {
+    public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) enableFullscreen();
     }
